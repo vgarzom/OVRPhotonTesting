fd773df14d7818546728339218db429213f27d96
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2019-10-23 17:11:18 -0500

    Updates OVRPlayer controller for using photonView on UpdateMovement

---
+++ Assets/Scripts/OVRPhotonPlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(PhotonView))]
+public class OVRPhotonPlayerController : OVRPlayerController
+{
+
+    [Tooltip("PhotonView to ...")]
+    public PhotonView photonView;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    override public void UpdateMovement()
+    {
+        if (photonView.IsMine)
+        {
+            base.UpdateMovement();
+        }
+        else {
+            return;
+        }
+    }
+
+}
